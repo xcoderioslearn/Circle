2340623e37321f5b7352edf164e694271216f977
Your Name <you@example.com>
2026-03-02 12:22:40 +0000

    circles overlapping

--- circle_mesh_array.gd
+++ circle_mesh_array.gd
@@ -6,21 +6,21 @@ func _ready() -> void:
 	var my_vertices = PackedVector3Array()
 	var my_colors = PackedColorArray()
 	
-	var current_indices_iteration = 0
-	
 	var point := Vector3.RIGHT
-	var steps := 17
+	var steps := 18
 	var angle_step := TAU / steps
-	for i in steps + 1 :
+	for i in steps :
+		my_vertices.append(Vector3.ZERO)
+		point = Vector3.LEFT.rotated(Vector3.UP, angle_step * i)
 		my_vertices.append(point)
-		point = point.rotated(Vector3.UP, angle_step)
+		point = (Vector3.LEFT.rotated(Vector3.UP, angle_step * (i + 1)))
+		my_vertices.append(point)
 		var color := Color.from_hsv(randf(), 0.8, randf())
 		my_colors.append(color)
+		my_colors.append(color)
+		my_colors.append(color)
 
-	
-	
 	surface_array[Mesh.ARRAY_VERTEX] = my_vertices
 	surface_array[Mesh.ARRAY_COLOR] = my_colors
 	  
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
-	print(my_colors)
--- circle_mesh_array_2.gd
+++ circle_mesh_array_2.gd
@@ -1,25 +1,41 @@
 extends MeshInstance3D
 
-func _ready():
+var t := 1.5
+
+func _ready() -> void:
 	var arrays := []
 	arrays.resize(Mesh.ARRAY_MAX)
 	arrays[Mesh.ARRAY_VERTEX] = PackedVector3Array()
 	arrays[Mesh.ARRAY_COLOR] = PackedColorArray()
-	
-	var steps := 18
-	var angle_step := TAU / steps
-	for i in steps:
-		arrays[Mesh.ARRAY_VERTEX].push_back(Vector3.ZERO)
-		arrays[Mesh.ARRAY_VERTEX].push_back(Vector3.LEFT.rotated(Vector3.UP, angle_step * i))
-		arrays[Mesh.ARRAY_VERTEX].push_back(Vector3.LEFT.rotated(Vector3.UP, angle_step * (i + 1)))
-		if i%3 == 0:
-			var color := Color.WHITE
-			arrays[Mesh.ARRAY_COLOR].append_array([color, color, color])
-		elif i%2 == 0: 
-			var color := Color.from_hsv(randf(), 0.8, randf())
-			arrays[Mesh.ARRAY_COLOR].append_array([color, color, color])
-			arrays[Mesh.ARRAY_COLOR].append_array([color, color, color])
-		elif  i%1 == 0 and i%3 != 0:
-			pass
 
+	var circle_steps := 24
+	var circle := PackedVector3Array()
+	for i in circle_steps:
+		circle.push_back(Vector3.ZERO)
+		circle.push_back(Vector3.RIGHT.rotated(Vector3.DOWN, i * TAU / circle_steps))
+		circle.push_back(Vector3.RIGHT.rotated(Vector3.DOWN, (i + 1) * TAU / circle_steps))
+	for n in circle_steps +18:
+		circle.remove_at(3)
+
+	var color := PackedColorArray()
+	color.resize(circle.size())
+	color.fill(Color.WHITE)
+	
+	
+	arrays[Mesh.ARRAY_VERTEX].append_array(circle)
+	arrays[Mesh.ARRAY_COLOR].append_array(color)
+
+
+	var circle_count = 6
+	for i in circle_count:
+		var transform_circle := Transform3D().translated(Vector3.RIGHT * abs(sin(t))).rotated(Vector3.UP, i * TAU / circle_count)
+		arrays[Mesh.ARRAY_VERTEX].append_array(transform_circle * circle)
+		color.fill(Color.from_hsv(float(i) / circle_count, 1.0, 0.7))
+		arrays[Mesh.ARRAY_COLOR].append_array(color)
+	print(arrays[Mesh.ARRAY_VERTEX].size())
+	for n in circle_count * 2 +18:
+		arrays[Mesh.ARRAY_VERTEX].remove_at(0)
+		arrays[Mesh.ARRAY_COLOR].remove_at(0)
+	mesh = ArrayMesh.new()
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	print(arrays[Mesh.ARRAY_VERTEX])
